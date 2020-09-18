1bf73f379c1ee1c49ce4f3e67c924777d13bd44f
KatherineMak <30404996+KatherineMak@users.noreply.github.com>
2020-09-18 15:43:47 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import './js/main.js';
+import './sass/normalize.sass';
+import './sass/sidebar.sass';
+import './sass/header.sass';
+import './sass/base.sass';
+import './sass/form.sass';
+import './sass/media.sass';
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.sass$/,
+                use: [
+                    miniCss.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new miniCss({
+            filename: 'style.css'
+        })
+    ]
+};
